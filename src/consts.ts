afeec00926277a04f027744ea96330a3bb9e8115
Konstantin Kovar <mail@vomkonstant.in>
2024-03-10 12:25:10 +0100

    added sidenote component and tried it on content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Konstantin Kovar';
+export const SITE_DESCRIPTION = 'Geiler Typ und Softwareentwickler';
+
+export const generateUuid = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & (0x3 | 0x8));
+    return v.toString(16);
+  });
+}
