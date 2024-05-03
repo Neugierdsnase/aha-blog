0d94e0fc2b57182bdde50a11d8cfe5f434a8f843
Konstantin Kovar <mail@vomkonstant.in>
2024-05-03 18:57:24 +0200

    heading, and marginnotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import htmx from "astro-htmx";
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.vomkonstant.in",
   markdown: {
     syntaxHighlight: false,
     gfm: false,
-    remarkPlugins: ["remark-frontmatter", "remark-mdx-frontmatter"],
+    remarkPlugins: ["remark-frontmatter", "remark-mdx-frontmatter"]
   },
-  integrations: [mdx(), sitemap(), htmx()],
+  integrations: [mdx(), sitemap(), htmx(), alpinejs()]
 });
