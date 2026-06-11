7d50e38aa0b50fa35986b707dfd4934d7be99530
Konstantin Kovar <konstantin.kovar@willhaben.at>
2026-06-11 08:14:09 +0200

    added react plugin(s)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,18 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.vomkonstant.in",
-	markdown: {
-		syntaxHighlight: false,
-		gfm: true,
-		rehypePlugins: [],
-	},
-	integrations: [mdx(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    site: "https://blog.vomkonstant.in",
+    markdown: {
+        syntaxHighlight: false,
+        gfm: true,
+        rehypePlugins: [],
+    },
+    integrations: [mdx(), sitemap(), react()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
