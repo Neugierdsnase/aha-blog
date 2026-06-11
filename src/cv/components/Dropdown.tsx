88661455b2a232e77d0883260d32ca6e234522d6
Konstantin Kovar <konstantin.kovar@willhaben.at>
2026-06-11 15:24:33 +0200

    changed shadcn theme and swapped out Dropdown component

--- src/cv/components/Dropdown.tsx
+++ src/cv/components/Dropdown.tsx
@@ -1,11 +1,17 @@
 import type { TagType } from '../types';
 import {
-  useState,
   type Dispatch,
   type FunctionComponent,
   type SetStateAction,
 } from 'react';
-import { Check, Funnel } from 'lucide-react';
+import { Check } from 'lucide-react';
+import {
+  Combobox,
+  ComboboxContent,
+  ComboboxInput,
+  ComboboxItem,
+  ComboboxList,
+} from "@/components/ui/combobox"
 
 type DropdownProps = {
   options: TagType[];
@@ -13,12 +19,7 @@ type DropdownProps = {
   setActiveFilters: Dispatch<SetStateAction<TagType[]>>;
 };
 
-export const Dropdown: FunctionComponent<DropdownProps> = ({
-  options,
-  activeFilters,
-  setActiveFilters,
-}) => {
-  const [isOpen, setIsOpen] = useState(false);
+export const Dropdown: FunctionComponent<DropdownProps> = ({ options, activeFilters, setActiveFilters }) => {
   const handleFilterClick = (option: TagType) => {
     setActiveFilters((prev) => {
       if (prev.includes(option)) {
@@ -30,51 +31,23 @@ export const Dropdown: FunctionComponent<DropdownProps> = ({
   };
 
   return (
-    <div id="filter-dropdown-menu" className="dropdown-menu">
-      <button
-        type="button"
-        id="filter-dropdown-menu-trigger"
-        aria-haspopup="menu"
-        aria-controls="filter-dropdown-menu-menu"
-        aria-expanded="false"
-        className="btn-outline"
-        onClick={() => setIsOpen((p) => !p)}
-      >
-        <Funnel />
-        Filter
-      </button>
-      {isOpen && (
-        <div data-popover className="min-w-56">
-          <div role="menu">
-            <div role="group" aria-labelledby="filter-options">
-              {options &&
-                options.map((op) => {
-                  const isChecked = activeFilters.includes(op);
+    <Combobox items={options}>
+      <ComboboxInput placeholder="Filter" />
+      <ComboboxContent>
+        <ComboboxList>
+          {(item) => {
+            const isChecked = activeFilters.includes(item);
 
-                  return (
-                    <div
-                      key={op}
-                      role="menuitemcheckbox"
-                      className="w-full"
-                      data-value={op}
-                    >
-                      <button
-                        type="button"
-                        className="flex h-full w-full gap-2"
-                        onClick={() => handleFilterClick(op)}
-                      >
-                        <div className="flex size-4 items-center justify-center">
-                          {isChecked && <Check />}
-                        </div>
-                        {op}
-                      </button>
-                    </div>
-                  );
-                })}
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-};
+            return (
+              <ComboboxItem key={item} value={item} onClick={() => handleFilterClick(item)}>
+                {isChecked && <Check />}
+                {item}
+              </ComboboxItem>
+            )
+          }}
+        </ComboboxList>
+      </ComboboxContent>
+    </Combobox>
+  )
+}
+
